3ac9eef15684c2eaffabe30bb4d72a001279b06d
PassCoded <griffin3579615@protonmail.com>
2023-12-14 00:46:28 +0100

    update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def load_data(file_path, delim):
 # charge les données CSV dans les variables.
 donnees = load_data('RTE_2020.csv', ',')
 
-choix = int(input("1: consommation / 2: énergies / 3: échanges / 4 taux de Co2:"))
+choix = int(input("1: consommation / 2: énergies / 3: échanges / 4 taux de Co2: "))
 
 titre = ""
 label_nom = []
@@ -44,48 +44,51 @@ else:
     raise ValueError("Cet index ne fait pas partie de la liste donnée.")
 
 
-def interpolate(value, base):
-    value = max(0, min(value, base))
-    factor = value / base
-    return factor
-
-
-def get_x_from_date(date):
-    year, month, day = map(int, date.split('-'))
-    maxd = 30
-    if month in {1, 3, 5, 7, 8, 10, 12}:
-        maxd = 31
-    return year + (interpolate(month, 12)) + (interpolate(day, maxd) * 0.1)
-
 # fonction qui retourne True si la liste contiens un élément vide.
 def has_empty(liste):
     return any(value == '' or value is None for value in liste)
 
 
 y_dic = {}
+cam_dic = {}
 x = []
-y = []
-p = 0
 for data in donnees:
     if has_empty(data):
         continue
-    x.append(mdates.datestr2num(data[2]))
-    for i in indexs:
-        if i not in y_dic:
-            y_dic[i] = [(int(data[i]))]
-        else:
-            y_dic.get(i).append(int(data[i]))
 
-# configuration du graphique et affichage.
-plt.figure(figsize=(18, 10))
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30))
-plt.gcf().autofmt_xdate()
+    x.append(mdates.datestr2num(data[2]))
+
+    for i in indexs:
+        y_dic.setdefault(i, []).append(int(data[i]))
+        cam_dic[i] = cam_dic.get(i, 0) + int(data[i])
+
+values = list(cam_dic.values())
+total = sum(values)
+pourcentages = [val / total for val in values]
+
+# Configuration du graphique et affichage du line plot.
+fig, ax = plt.subplots(figsize=(18, 10))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=30))
+fig.autofmt_xdate()
+
 for val in y_dic.values():
-    plt.plot(x, val, linewidth=0.5)
-plt.title(titre)
-plt.xlabel('Mois')
-plt.ylabel('Utilisation')
-plt.legend(label_nom)
-frame = plt.gca()
+    ax.plot(x, val, linewidth=0.5)
+
+ax.set_title(titre)
+ax.set_xlabel('Mois')
+ax.set_ylabel('Utilisation')
+ax.legend(label_nom)
+
+# Afficher le premier graphique
+plt.show()
+
+# Create a new figure for the pie chart
+fig_pie, ax_pie = plt.subplots(figsize=(10, 8))
+
+# Plot the pie chart
+ax_pie.pie(pourcentages, labels=label_nom, autopct='%1.1f%%')
+ax_pie.set_title("Part de production")
+plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.15))
+# Afficher le deuxieme graphique
 plt.show()
